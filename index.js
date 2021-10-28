97e25fe4e7c99102b812b99e165426a5cd177fe0
datanh98hp <dat198hp@example.com>
2021-10-28 16:09:53 +0700

    express is added

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-const http = require('http');
-const data = require('./data')
-http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'application/json'});
-    res.write(JSON.stringify(data.users));
-}).listen(45005)
+const http = require("http");
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const publicPath = path.join(__dirname, "public");
+
+// app.use(express.static(publicPath))
+app.set('view engine','ejs') // set engine 
+
+app.get("/", (req, res) => {
+  res.sendFile(`${publicPath}/index.html`);
+});
+//
+app.get("/about", (req, res) => {
+  res.sendFile(`${publicPath}/about.html`);
+});
+// 
+app.get("/profile", (req, res) => {
+  const user = {
+    name :'Dat Anh',
+    email:'dat198hp@gmail.com',
+    city:'Hai Phong',
+    skills:['php','js','HTML']
+  }
+  res.render('profile',{user});
+});
+
+// 
+app.get('/login',(req,res)=>{
+  res.render('login')
+})
+// Error page 
+app.get("*", (req, res) => {
+    res.sendFile(`${publicPath}/nopage.html`);
+  });
 
 
+
+app.listen(3000);
---
+++ views/common/Header.js
@@ -0,0 +1,3 @@
+<nav>
+    <h4> Header File</h4>
+</nav>
